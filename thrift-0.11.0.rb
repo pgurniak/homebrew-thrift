f907d8c2d22b942b9321e125989d7bc02e7cd16c
Paul Gurniak <GurniakP@valassis.com>
2019-06-10 07:29:19 -0400

    Copy/tweak thrift 0.11 formula from homebrew-core

---
+++ thrift-0.11.0.rb
@@ -0,0 +1,56 @@
+class Thrift0110 < Formula
+  desc "Framework for scalable cross-language services development"
+  homepage "https://thrift.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=/thrift/0.11.0/thrift-0.11.0.tar.gz"
+  sha256 "c4ad38b6cb4a3498310d405a91fef37b9a8e79a50cd0968148ee2524d2fa60c2"
+
+  head do
+    url "https://github.com/apache/thrift.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+  end
+
+  depends_on "bison" => :build
+  depends_on "boost"
+  depends_on "openssl"
+
+  def install
+    system "./bootstrap.sh" unless build.stable?
+
+    args = %W[
+      --disable-debug
+      --disable-tests
+      --prefix=#{prefix}
+      --libdir=#{lib}
+      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --without-erlang
+      --without-haskell
+      --without-java
+      --without-perl
+      --without-php
+      --without-php_extension
+      --without-python
+      --without-ruby
+    ]
+
+    ENV.cxx11 if ENV.compiler == :clang
+
+    # Don't install extensions to /usr:
+    ENV["PY_PREFIX"] = prefix
+    ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
+
+    system "./configure", *args
+    ENV.deparallelize
+    system "make"
+    system "make", "install"
+    mv bin/"thrift", bin/"thrift-0.11.0"
+  end
+
+  test do
+    system "#{bin}/thrift-0.11.0", "--version"
+  end
+end
